e80a60fc510d2fa1e6e407087a840b426447ef55
Tom Martin <tom@tpgmartin.com>
2017-07-14 11:25:03 +0100

    Start console application, basic read from/write to console

---
+++ KeyValueStoreConsoleApplication/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace KeyValueStoreConsoleApplication
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = typeof(MainClass).Name;
+            Run();
+        }
+
+        static void Run()
+        {
+            while (true)
+            {
+                var consoleInput = ReadFromConsole();
+                if (string.IsNullOrWhiteSpace((consoleInput))) continue;
+
+                try
+                {
+                    string result = Execute(consoleInput);
+                    WriteToConsole(result);
+                }
+                catch (Exception ex)
+                {
+                    WriteToConsole(ex.Message);
+                }
+            }
+        }
+
+        static string Execute(string command)
+        {
+            return string.Format("Executed the {0} Command", command);
+        }
+
+        public static void WriteToConsole(string message = "")
+        {
+            if(message.Length > 0)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        const string _prompt = "keyvaluestore>";
+        public static string ReadFromConsole(string promptMessage = "")
+        {
+            Console.Write(_prompt + promptMessage);
+            return Console.ReadLine();
+        }
+    }
+}
